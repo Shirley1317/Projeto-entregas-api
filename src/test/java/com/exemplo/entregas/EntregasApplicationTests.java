772c8fd4451f7d7636fdaef8dea68f945b9fd196
Shirley Esteves <estevesshirley@gmail.com>
2024-11-18 18:02:54 -0300

    Initial Commit

---
+++ src/test/java/com/exemplo/entregas/EntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
